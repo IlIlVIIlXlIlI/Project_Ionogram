8ddb44113f8ffbc23dda8a8ee27079d4e4e09780
Shogo <vanitas710@yahoo.co.jp>
2021-03-13 23:29:07 +0900

    Initial Commit

---
+++ Ionogram/IonogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  IonogramApp.swift
+//  Ionogram
+//
+//  Created by Shogo Nobuhara on 2021/03/13.
+//
+
+import SwiftUI
+
+@main
+struct IonogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
