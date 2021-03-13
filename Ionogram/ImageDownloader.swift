2134bdaa9aa24619cbe8dc151cd469007ff9e2a9
Shogo <vanitas710@yahoo.co.jp>
2021-03-14 02:26:46 +0900

    イオノグラム公式サイトから画像を読み込めるようにした。

---
+++ Ionogram/ImageDownloader.swift
@@ -0,0 +1,24 @@
+//
+//  ImageDownloader.swift
+//  Ionogram
+//
+//  Created by Shogo Nobuhara on 2021/03/14.
+//
+
+import Foundation
+
+class ImageDownloader : ObservableObject {
+    @Published var downloadData: Data? = nil
+
+    func downloadImage(url: String) {
+
+        guard let imageURL = URL(string: url) else { return }
+
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: imageURL)
+            DispatchQueue.main.async {
+                self.downloadData = data
+            }
+        }
+    }
+}
---
+++ Ionogram/URLImage.swift
@@ -0,0 +1,39 @@
+//
+//  URLImage.swift
+//  Ionogram
+//
+//  Created by Shogo Nobuhara on 2021/03/14.
+//
+
+import SwiftUI
+
+struct URLImage: View {
+
+    let url: String
+    @ObservedObject private var imageDownloader = ImageDownloader()
+
+    init(url: String) {
+        self.url = url
+        self.imageDownloader.downloadImage(url: self.url)
+    }
+
+    var body: some View {
+        if let imageData = self.imageDownloader.downloadData {
+            let img = UIImage(data: imageData)
+            return VStack {
+                Image(uiImage: img!).resizable()
+            }
+        } else {
+            return VStack {
+                Image(uiImage: UIImage(systemName: "icloud.and.arrow.down")!).resizable()
+            }
+        }
+    }
+}
+
+struct URLImage_Previews: PreviewProvider {
+    static var previews: some View {
+        URLImage(url: "https://wdc.nict.go.jp/ionog/ionogram/nowpng/png/allsite.png")
+            .aspectRatio(contentMode: .fit)
+    }
+}
